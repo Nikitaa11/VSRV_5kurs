9fd35c3523c3c2861a5103b4e87af8765ce4e04a
nikita <asta-nik007@mail.ru>
2024-09-26 13:21:20 +0300

    in_out

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
+#define LEN 30
 
 int main() {
-    printf("Hello, World");
+    char name[LEN];
+    int i = 0;
+    while (name[i - 1] != '\n') {
+        scanf("%c", &name[i]);
+        i++;
+    }
+    printf("Hello, ");
+    for (int j = 0; j < i; j++) {
+        printf("%c", name[j]);
+    }
+    
     return 0;
 }
